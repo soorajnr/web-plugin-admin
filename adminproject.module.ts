fee03aac65861848248aba695154fa2ae62fd6a6
navarsha-neork <125448610+navarsha-neork@users.noreply.github.com>
2024-04-27 20:42:17 +0530

    #repo initial setup

---
+++ adminproject.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RoleComponent } from './role/role.component';
+
+
+
+@NgModule({
+  declarations: [
+    RoleComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminprojectModule { }
